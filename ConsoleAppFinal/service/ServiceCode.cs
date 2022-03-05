65fc39133585bb0e937d8b814ca3eb641910bea7
jahangir <98384389+Jahangiresh@users.noreply.github.com>
2022-03-05 17:50:34 +0400

    Add files via upload

---
+++ ConsoleAppFinal/service/ServiceCode.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFinal
+{
+    class ServiceCode : IServiceCode
+    {
+        private List<Group> _groups = new List<Group>();
+        public List<Group> Groups => _groups;
+        private List<Student> _students => new List<Student>();
+        public List<Student> Students => _students;
+        public bool CheckGroup(string no)
+        {
+            for (int i = 0; i < no.Length; i++)
+            {
+
+                if (no.Length == 4 && char.IsUpper(no[0]) && char.IsDigit(no[++i]))
+                {
+                    return true;
+                }
+
+                else
+                {
+                    Console.WriteLine("Qrup adi 1 boyuk her ve 3 reqemden ibaret olmalidir"); break;
+                }
+            }
+            return false;
+        }
+        public bool CheckStudentName(string fullname)
+        {
+            if (fullname.Length==2)
+            {
+                string[] subs = fullname.Split(' ');
+                string name = subs[0];
+                string surname = subs[1];
+                //string name1 = "";
+                //string surname1 = "";
+                fullname = name + ' ' + surname;
+                for (int i = 0; i < name.Length; i++)
+                {
+
+                    if (char.IsUpper(name[0]) && char.IsLower(name[++i]))
+                    {
+                        name = subs[0];
+                    }            
+                }
+                for (int i = 0; i < surname.Length; i++)
+                {
+                    if ((char.IsUpper(surname[0]) && char.IsLower(surname[++i])))
+                    {
+                        surname = subs[1];
+                    }
+                }
+                if (name + surname == subs[0] + subs[1])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public string CreateGroup(int limit,Category categories, string no, bool isOnline )
+        {
+           
+            CheckGroup(no);
+
+
+            if (isOnline)
+            {
+                limit = 10;
+            }
+            if (!isOnline)
+            {
+                limit = 10;
+            }
+            Group group = new Group(limit,categories, no,isOnline);
+            Groups.Add(group);
+            return group.No;
+        }
+
+       
+        public void ShowGroups()
+        {
+            if (_groups.Count == 0)
+            {
+                Console.WriteLine("there is no group in this Academy");
+                return;
+            }
+            foreach (Group group in _groups)
+            {
+                Console.WriteLine(group);
+            }
+        }
+
+        public void EditGroupName(string no, string newNo)
+        {
+            Group existedGroup = FindGroup(no);
+
+            if (existedGroup == null)
+            {
+                Console.WriteLine("plese choose correct grup num");
+                return;
+            }
+
+            foreach (Group group in _groups)
+            {
+                if (group.No == newNo)
+                {
+                    Console.WriteLine($"{newNo} group is already exist");
+                }
+            }
+            existedGroup.No = newNo;
+            Console.WriteLine($"{no} succesfuly changed to {newNo}");
+        }
+        public Group FindGroup(string no)
+        {
+            foreach (Group group in _groups)
+            {
+                if (group.No == no)
+                {
+                    return group;
+                }
+            }
+            return null;
+        }
+
+        public void GetGroupStudents(string no)
+        {
+            Group group = FindGroup(no);
+            if (group == null)
+            {
+                Console.WriteLine("pls choosevalid group no");
+                return;
+            }
+            if (Students.Count!=0)
+            {
+                foreach (Student student in group.Students)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+            else
+            {
+                Console.WriteLine("telebe yoxdu");
+            }
+            
+        }
+        public void GetAllStudents(List<Student> Students)
+        {
+            if (Students.Count!=0)
+            {
+                foreach (Student student in Students)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+            else
+            {
+                Console.WriteLine("kursda telebe yoxdi");
+            }
+           
+        }
+
+
+        public string CreateStudent(string fullname, string groupno)
+        {
+            if (fullname.Length == 2)
+            {
+                CheckStudentName(fullname);
+            }
+            else
+            {
+                return "ad ve ya soyad sehv yazilib";
+            }
+            Student student = new Student(fullname, groupno);
+            Group group = Groups.Find(x => x.No.Trim().ToLower() == groupno.Trim().ToLower());
+            group.Students.Add(student);
+            Students.Add(student);
+            return student.Fullname;
+
+        }
+
+      
+
+     
+    }
+}
---
+++ ConsoleAppFinal/service/ServiceMenu.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFinal.service
+{
+    static class ServiceMenu
+    {
+        public static ServiceCode serviceCode=new ServiceCode();
+        public static void CreateGroupMenu()
+        {
+           
+            Console.WriteLine("qrup no daxil edin");
+            string no;
+            no = Console.ReadLine();
+            
+
+            if (serviceCode.CheckGroup(no))
+            {
+                int limit;
+                bool isOnline = false;
+                Console.WriteLine("1.online\n2.offline");
+                string strLimit = Console.ReadLine();
+                bool resultLimit = int.TryParse(strLimit, out limit);
+
+                switch (limit)
+                {
+                    case 1:
+                        isOnline = true;
+                        break;
+                    case 2:
+                        isOnline = false;
+                        break;
+                    default:
+                        Console.WriteLine("choose fucking valid num! fucking 1 or fucking 2 PLEASE ");
+                        break;
+                }
+
+                foreach (Category c in System.Enum.GetValues(typeof(Category)))
+                {
+                    Console.WriteLine($"{(int)c} {c}");
+                }
+                int category;
+                string catStr = Console.ReadLine();
+                bool resultCat = int.TryParse(catStr, out category);
+               
+                if (resultCat)
+                {
+
+                    switch (category)
+                    {
+                        
+                        case (int)Category.Programming:
+                          
+                            string No = serviceCode.CreateGroup(limit, Category.Programming, no, isOnline);
+                            Console.WriteLine($"{No} group succesfully created");
+                            break;
+                        case (int)Category.Design:
+                       
+                             No = serviceCode.CreateGroup(limit, Category.Design, no, isOnline);
+                            Console.WriteLine($"{No} group succesfully created");
+                            break;
+                        case (int)Category.System_Administration:
+                          
+                             No = serviceCode.CreateGroup(limit, Category.System_Administration, no, isOnline);
+                            Console.WriteLine($"{No} group succesfully created");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("please choose valid category");
+                }
+            }
+
+        }
+        public static void EditGroupMenu()
+        {
+            Console.WriteLine("qrupun adini yaz");
+            string no = Console.ReadLine();
+            Console.WriteLine("yeni qrup adi yaz");
+            string newNo = Console.ReadLine();
+           serviceCode.EditGroupName(no, newNo);
+        }
+        public static void ShowGroupsMenu()
+        {
+            serviceCode.ShowGroups();
+        }
+        public static void GetGroupStudentsMenu()
+        {
+            Console.WriteLine("qrupun adini yaz");
+            string no = Console.ReadLine();
+            serviceCode.GetGroupStudents(no);
+        }
+        public static void GetAllStudentsMenu()
+        {
+            
+            serviceCode.GetAllStudents(serviceCode.Students);
+        }
+        public static void CreateStudentMenu()
+        {
+           
+            Console.WriteLine("Telebenin ad soyadi");
+            string fullname = Console.ReadLine();
+            if (serviceCode.CheckStudentName(fullname))
+            {
+                Console.WriteLine("zzor");
+            }
+            else
+            {
+                Console.WriteLine("yox sehvdi");
+            }
+        }
+    }
+}
